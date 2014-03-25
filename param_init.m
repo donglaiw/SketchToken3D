20cf23ad7dd6f889762c286241cdd103e953e987
Eminemya <weiddoonngglai@gmail.com>
2014-03-25 12:01:10 -0400

    clusters

--- param_init.m
+++ param_init.m
@@ -1,5 +1,5 @@
-D_STRACK = '/data/vision/billf/manifold-learning/Data/Seg/Segtrack/';
-D_ST3D = '/data/vision/billf/deep-learning/SketchToken3D/';
-D_VLIB = '/data/vision/billf/stereo-vision/VisionLib/Donglai/';
-D_BERK1 = '/data/vision/billf/deep-learning/data/Berk_occ/train/';
-D_BERK2 = '/data/vision/billf/deep-learning/data/Berk_occ/test/';
+D_STRACK = '/home/Stephen/Desktop/Data/Seg/Segtrack/'; 
+D_ST3D = '/home/Stephen/Desktop/SketchToken3D/';
+D_BERK1 = '/home/Stephen/Desktop/Data/Occ/Berk/train/';
+D_BERK2 = '/home/Stephen/Desktop/Data/Occ/Berk/test/';
+D_VLIB = '/home/Stephen/Desktop/VisionLib/Donglai/';

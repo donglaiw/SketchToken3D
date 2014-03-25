de61f4030930ce98e3f540515f47c2fad6e29fec
Eminemya <weiddoonngglai@gmail.com>
2014-03-25 12:01:43 -0400

    .gitignore is now working

--- param_init.m
+++
@@ -1,5 +0,0 @@
-D_STRACK = '/home/Stephen/Desktop/Data/Seg/Segtrack/'; 
-D_ST3D = '/home/Stephen/Desktop/SketchToken3D/';
-D_BERK1 = '/home/Stephen/Desktop/Data/Occ/Berk/train/';
-D_BERK2 = '/home/Stephen/Desktop/Data/Occ/Berk/test/';
-D_VLIB = '/home/Stephen/Desktop/VisionLib/Donglai/';
